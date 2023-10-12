152cdfa464dd4195607379d170d644c669238b1d
Anton_Dementev <anton_dem@MacBook-Air--AntonDementev.local>
2023-10-13 01:06:18 +0300

    Добавил тесты

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,19 @@
 import unittest
+import sys
+sys.path.append(r'/Users/anton_dem/Desktop/Labs/Proga/cs-120')
+from src.lab1.calculator import plus, minus, multiply, divide
 
 class CalculatorTestCase(unittest.TestCase):
 
     # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+    def test_plus(self):
+        self.assertEquals(plus(2, 2), f"Сумма: {4}")
+    
+    def test_minus(self):
+        self.assertEquals(minus(5, 2), f"Разность: {3}")
+    
+    def test_multiply(self):
+        self.assertEquals(multiply(5, 2), f"Произведение: {10}")
+    
+    def test_divide(self):
+        self.assertEquals(divide(10, 2), f"Частное: {float(5)}")
