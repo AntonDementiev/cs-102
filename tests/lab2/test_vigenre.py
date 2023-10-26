6aee79d717a80d535a9f0b393f39576b38a1e4c2
Anton_Dementev <anton_dem@MacBook-Air--AntonDementev.local>
2023-10-26 18:50:57 +0300

    Добавлены новые тесты для Виженера

--- tests/lab2/test_vigenre.py
+++ tests/lab2/test_vigenre.py
@@ -1,7 +1,16 @@
 import unittest
+import random
+import string
 from src.lab2.vigenre import encrypt_vigenere, decrypt_vigenere
 
 class VigenereTestCase(unittest.TestCase):
+
+    def test_randomized(self):
+        kwlen = random.randint(4, 24)
+        keyword = ''.join(random.choice(string.ascii_letters) for _ in range(kwlen))
+        plaintext = ''.join(random.choice(string.ascii_letters + ' -,') for _ in range(64))
+        ciphertext = encrypt_vigenere(plaintext, keyword)
+        self.assertEqual(plaintext, decrypt_vigenere(ciphertext, keyword))
 
     def test_encrypt_vigenere_upper(self):
         self.assertEqual(encrypt_vigenere(plaintext="PYTHON", keyword="A"), "PYTHON")
@@ -20,3 +29,4 @@ class VigenereTestCase(unittest.TestCase):
     
     def test_decrypt_vigenere_lemon(self):
         self.assertEqual(decrypt_vigenere(ciphertext="LXFOPVEFRNHR", keyword="LEMON"), 'ATTACKATDAWN')
+
